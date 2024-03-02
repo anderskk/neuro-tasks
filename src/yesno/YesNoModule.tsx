f350e8fad31713861d08d000e32dd1a6e790e71c
Anders Kristiansen <andersk.kristiansen@gmail.com>
2024-03-02 15:28:25 +0100

    draggable yesno w local storage

--- src/yesno/YesNoModule.tsx
+++ src/yesno/YesNoModule.tsx
@@ -1,20 +1,38 @@
 import { YesNoRightLeft } from './YesNoRightLeft.tsx';
 import { YesNoAll } from './YesNoAll.tsx';
 import { YesNoTopBottom } from './YesNoTopBottom.tsx';
+import Draggable, { ControlPosition } from 'react-draggable';
+import { useMemo } from 'react';
 
+const yesNoPositionKey = 'yesno-position';
+const savePosition = (position: ControlPosition) => localStorage.setItem(yesNoPositionKey, JSON.stringify(position))
+
+const firstDefaultPosition: ControlPosition = {x: 200, y: 100}
 
 export const YesNoModule = () => {
+  const defaultPosition: ControlPosition = useMemo(() =>
+      JSON.parse(localStorage.getItem(yesNoPositionKey) || JSON.stringify(firstDefaultPosition)),
+    []
+  );
+
   return (
-    <div className="h-full">
-      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col space-y-[150px] items-center">
-        <YesNoTopBottom/>
-        <div className="flex space-x-[150px]">
-          <YesNoRightLeft/>
-          <YesNoAll/>
-          <YesNoRightLeft/>
+    <div className="h-full relative">
+      <Draggable
+        bounds="parent"
+        defaultPosition={defaultPosition}
+        onStop={(_, {x, y}) => savePosition({x, y})}
+      >
+        <div
+          className="flex flex-col space-y-[150px] items-center w-min cursor-move">
+          <YesNoTopBottom/>
+          <div className="flex space-x-[150px]">
+            <YesNoRightLeft/>
+            <YesNoAll/>
+            <YesNoRightLeft/>
+          </div>
+          <YesNoTopBottom/>
         </div>
-        <YesNoTopBottom/>
-      </div>
+      </Draggable>
     </div>
   )
 }
--- src/yesno/YesNoRightLeft.tsx
+++ src/yesno/YesNoRightLeft.tsx
@@ -1,11 +1,5 @@
-
+import { YesNoTopBottom } from './YesNoTopBottom';
 
 export const YesNoRightLeft = () => {
-  return (
-    <div className="relative w-[100px] h-[100px] border-[2px] border-foreground rounded-full bg-transparent">
-      <div className="absolute top-[40px] left-[40px] w-[20px] h-[20px] rounded-full bg-red-600"/>
-      <div className="absolute top-[40px] left-[-10px] w-[20px] h-[20px] rounded-full bg-foreground"/>
-      <div className="absolute top-[40px] right-[-10px] w-[20px] h-[20px] rounded-full bg-foreground"/>
-    </div>
-  )
+  return <YesNoTopBottom className="rotate-90" />
 }
