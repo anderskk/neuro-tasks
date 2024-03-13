d227bdd2c664b502f6ff287d9b6665778a96ad3d
Anders Kristiansen <andersk.kristiansen@gmail.com>
2024-03-13 22:08:58 +0100

    crosshair in circle

--- src/pursuit/components/Circle.tsx
+++ src/pursuit/components/Circle.tsx
@@ -1,4 +1,4 @@
-import clsx from 'clsx';
+import { cn } from '@utils';
 
 interface Props {
   radius: number;
@@ -6,9 +6,12 @@ interface Props {
 }
 
 export const Circle = ({ radius, className }: Props) => {
-  const classList = clsx(`rounded-full`, className);
+  const classList = cn('rounded-full relative', className);
 
   return (
-    <div style={{ width: `${radius}px`, height: `${radius}px`}} className={classList} />
+    <div style={{ width: `${radius}px`, height: `${radius}px`}} className={classList}>
+      <div className="absolute w-[6%] h-full bg-background left-[50%] translate-x-[-50%]" />
+      <div className="absolute h-[6%] w-full bg-background top-[50%] translate-y-[-50%]" />
+    </div>
   );
 };
--- src/saccades/SaccadesModule.tsx
+++ src/saccades/SaccadesModule.tsx
@@ -7,196 +7,206 @@ import { GameConfig, OrientationConfig } from "./types/GameConfig.ts";
 import { ConfigCard } from './components/ConfigCard.tsx';
 import { useIdle } from '@uidotdev/usehooks';
 import { clsx } from 'clsx';
+import useLocalStorageState from '../lib/useLocalStorageState.ts';
 
 interface GameState {
-	state: "playing" | "notPlaying";
-	leftCircle: "show" | "hide";
-	rightCircle: "show" | "hide";
+  state: "playing" | "notPlaying";
+  leftCircle: "show" | "hide";
+  rightCircle: "show" | "hide";
 }
 
 const initGameState: GameState = {
-	state: "notPlaying",
-	leftCircle: "show",
-	rightCircle: "show",
+  state: "notPlaying",
+  leftCircle: "show",
+  rightCircle: "show",
 };
 
 const initHorizontalConfig: OrientationConfig = {
-	repetitions: 30,
-	circleColors: {
-		lightMode: {
-			center: "#000",
-			sides: "#328bff",
-		},
-		darkMode: {
-			center: "#fff",
-			sides: "#4aceff",
-		},
-	},
-	circleDistance: 6,
-	blinkDuration: 400,
-	pauseDuration: 400,
+  repetitions: 30,
+  circleColors: {
+    lightMode: {
+      center: "#000",
+      sides: "#328bff",
+    },
+    darkMode: {
+      center: "#fff",
+      sides: "#4aceff",
+    },
+  },
+  circleDistance: 6,
+  blinkDuration: 400,
+  pauseDuration: 400,
 };
 
 const initVerticalConfig: OrientationConfig = {
-	...initHorizontalConfig,
-	circleDistance: 3.2,
+  ...initHorizontalConfig,
+  circleDistance: 3.2,
 };
 
 const initGameConfig: GameConfig = {
-	orientation: "horizontal",
-	horizontal: initHorizontalConfig,
-	vertical: initVerticalConfig,
+  orientation: "horizontal",
+  horizontal: initHorizontalConfig,
+  vertical: initVerticalConfig,
 };
 
 function SaccadesModule() {
-	const isIdle = useIdle(500);
-	const { resolveTheme } = useTheme();
-	const [gameConfig, setGameConfig] = useState<GameConfig>(initGameConfig);
-	const [gameState, setGameState] = useState<GameState>(initGameState);
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  const  orientationConfig = useMemo(
-		() => gameConfig[gameConfig.orientation],
-		[gameConfig],
-	);
-	const leftCirclePosition: Vector3 = useMemo(() => {
-		return gameConfig.orientation === "horizontal"
-			? [orientationConfig.circleDistance, 0, 0]
-			: [0, orientationConfig.circleDistance, 0];
-	}, [gameConfig.orientation, orientationConfig.circleDistance]);
-	const circleColors = useMemo(() => {
-		return resolveTheme() === "light"
-			? orientationConfig.circleColors.lightMode
-			: orientationConfig.circleColors.darkMode;
-	}, [resolveTheme, orientationConfig]);
+  const isIdle = useIdle(500);
+  const {resolveTheme} = useTheme();
+  const [storedGameConfig, setGameConfig] = useLocalStorageState<GameConfig | Partial<GameConfig>>(
+    'saccadesConfig',
+    {
+      defaultValue: () => initGameConfig
+    }
+  )
+  const gameConfig: GameConfig = useMemo(() => ({
+    ...storedGameConfig,
+    ...initGameConfig,
+  }), [storedGameConfig])
+  const [gameState, setGameState] = useState<GameState>(initGameState);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  const orientationConfig = useMemo(
+    () => gameConfig[gameConfig.orientation],
+    [gameConfig],
+  );
+  const leftCirclePosition: Vector3 = useMemo(() => {
+    return gameConfig.orientation === "horizontal"
+      ? [orientationConfig.circleDistance, 0, 0]
+      : [0, orientationConfig.circleDistance, 0];
+  }, [gameConfig.orientation, orientationConfig.circleDistance]);
+  const circleColors = useMemo(() => {
+    return resolveTheme() === "light"
+      ? orientationConfig.circleColors.lightMode
+      : orientationConfig.circleColors.darkMode;
+  }, [resolveTheme, orientationConfig]);
 
-	const rightCirclePosition: Vector3 = useMemo(() => {
-		return gameConfig.orientation === "horizontal"
-			? [-orientationConfig.circleDistance, 0, 0]
-			: [0, -orientationConfig.circleDistance, 0];
-	}, [gameConfig.orientation, orientationConfig.circleDistance]);
+  const rightCirclePosition: Vector3 = useMemo(() => {
+    return gameConfig.orientation === "horizontal"
+      ? [-orientationConfig.circleDistance, 0, 0]
+      : [0, -orientationConfig.circleDistance, 0];
+  }, [gameConfig.orientation, orientationConfig.circleDistance]);
 
-	const startHideCircleInterval = useCallback(
-		() =>
-			setInterval(() => {
-				const random = Math.random();
-				if (random < 0.5) {
-					setGameState((prevState) => ({ ...prevState, leftCircle: "hide" }));
-				} else {
-					setGameState((prevState) => ({ ...prevState, rightCircle: "hide" }));
-				}
-			}, orientationConfig.blinkDuration + orientationConfig.pauseDuration),
-		[orientationConfig.blinkDuration, orientationConfig.pauseDuration],
-	);
+  const startHideCircleInterval = useCallback(
+    () =>
+      setInterval(() => {
+        const random = Math.random();
+        if (random < 0.5) {
+          setGameState((prevState) => ({...prevState, leftCircle: "hide"}));
+        } else {
+          setGameState((prevState) => ({...prevState, rightCircle: "hide"}));
+        }
+      }, orientationConfig.blinkDuration + orientationConfig.pauseDuration),
+    [orientationConfig.blinkDuration, orientationConfig.pauseDuration],
+  );
 
-	const startShowCirclesInterval = useCallback(
-		() =>
-			setInterval(() => {
-				setGameState((prevState) => ({
-					...prevState,
-					leftCircle: "show",
-					rightCircle: "show",
-				}));
-			}, orientationConfig.blinkDuration + orientationConfig.pauseDuration),
-		[orientationConfig.blinkDuration, orientationConfig.pauseDuration],
-	);
+  const startShowCirclesInterval = useCallback(
+    () =>
+      setInterval(() => {
+        setGameState((prevState) => ({
+          ...prevState,
+          leftCircle: "show",
+          rightCircle: "show",
+        }));
+      }, orientationConfig.blinkDuration + orientationConfig.pauseDuration),
+    [orientationConfig.blinkDuration, orientationConfig.pauseDuration],
+  );
 
-	const startGame = useCallback(() => {
-		setGameState((prevState) => ({ ...prevState, state: "playing" }));
-		let hideCircleIntervalId: number;
-		setTimeout(() => {
-			hideCircleIntervalId = startHideCircleInterval();
-		}, orientationConfig.pauseDuration);
-		const showCirclesIntervalId = startShowCirclesInterval();
-		const gameTime =
-			(orientationConfig.repetitions + 1) *
-			(orientationConfig.blinkDuration + orientationConfig.pauseDuration);
+  const startGame = useCallback(() => {
+    setGameState((prevState) => ({...prevState, state: "playing"}));
+    let hideCircleIntervalId: number;
+    setTimeout(() => {
+      hideCircleIntervalId = startHideCircleInterval();
+    }, orientationConfig.pauseDuration);
+    const showCirclesIntervalId = startShowCirclesInterval();
+    const gameTime =
+      (orientationConfig.repetitions + 1) *
+      (orientationConfig.blinkDuration + orientationConfig.pauseDuration);
 
-		setTimeout(() => {
-			clearInterval(hideCircleIntervalId);
-			clearInterval(showCirclesIntervalId);
-			setGameState(initGameState);
-		}, gameTime);
-	}, [
-		orientationConfig.repetitions,
-		orientationConfig.blinkDuration,
-		orientationConfig.pauseDuration,
-		startHideCircleInterval,
-		startShowCirclesInterval,
-	]);
+    setTimeout(() => {
+      clearInterval(hideCircleIntervalId);
+      clearInterval(showCirclesIntervalId);
+      setGameState(initGameState);
+    }, gameTime);
+  }, [
+    orientationConfig.repetitions,
+    orientationConfig.blinkDuration,
+    orientationConfig.pauseDuration,
+    startHideCircleInterval,
+    startShowCirclesInterval,
+  ]);
 
-	const isPlaying = gameState.state === "playing";
+  const isPlaying = gameState.state === "playing";
 
-	const toggleOrientation = () =>
-		setGameConfig((prevState) => ({
-			...prevState,
-			orientation:
-				prevState.orientation === "horizontal"
-					? "vertical"
-					: "horizontal",
-		}))
+  const toggleOrientation = () =>
+    setGameConfig((prevState) => ({
+      ...prevState,
+      orientation:
+        prevState.orientation === "horizontal"
+          ? "vertical"
+          : "horizontal",
+    }))
 
-	const onRepetitionChange = (num: number) => setGameConfig((prevState) => ({
-		...prevState,
-		[prevState.orientation]: {
-			...prevState[prevState.orientation],
-			repetitions: num
-		}
-	}))
+  const onRepetitionChange = (num: number) => setGameConfig(({
+    ...gameConfig,
+    [gameConfig.orientation]: {
+      ...gameConfig[gameConfig.orientation],
+      repetitions: num
+    }
+  }))
 
-	const onBlinkDurationChange = (num: number) => setGameConfig((prevState) => ({
-		...prevState,
-		[prevState.orientation]: {
-			...prevState[prevState.orientation],
-			blinkDuration: num
-		}
-	}))
+  const onBlinkDurationChange = (num: number) => setGameConfig(({
+    ...gameConfig,
+    [gameConfig.orientation]: {
+      ...gameConfig[gameConfig.orientation],
+      blinkDuration: num
+    }
+  }))
 
-	const onPauseDurationChange = (num: number) => setGameConfig((prevState) => ({
-		...prevState,
-		[prevState.orientation]: {
-			...prevState[prevState.orientation],
-			pauseDuration: num
-		}
-	}))
+  const onPauseDurationChange = (num: number) => setGameConfig(({
+    ...gameConfig,
+    [gameConfig.orientation]: {
+      ...gameConfig[gameConfig.orientation],
+      pauseDuration: num
+    }
+  }))
 
-	return (
-		<>
-			<Canvas className={clsx({
-				'cursor-none': isIdle && isPlaying,
-			})}>
-				<ambientLight intensity={1.5} />
-				{gameState.leftCircle === "show" && (
-					<MeshCircle
-						position={leftCirclePosition}
-						color={circleColors.sides}
-						radius={0.3}
-					/>
-				)}
-				<MeshCircle
-					position={[0, 0, 0]}
-					color={circleColors.center}
-					radius={0.3}
-				/>
-				{gameState.rightCircle === "show" && (
-					<MeshCircle
-						position={rightCirclePosition}
-						color={circleColors.sides}
-						radius={0.3}
-					/>
-				)}
-			</Canvas>
-			{!isPlaying && (
-				<ConfigCard
-					gameConfig={gameConfig}
-					toggleOrientation={toggleOrientation}
-					startGame={startGame}
-					onChangeRepetitions={onRepetitionChange}
-					onChangeBlinkDuration={onBlinkDurationChange}
-					onChangePauseDuration={onPauseDurationChange}
-				/>
-			)}
-		</>
-	);
+  return (
+    <>
+      <Canvas className={clsx({
+        'cursor-none': isIdle && isPlaying,
+      })}>
+        <ambientLight intensity={1.5}/>
+        {gameState.leftCircle === "show" && (
+          <MeshCircle
+            position={leftCirclePosition}
+            color={circleColors.sides}
+            radius={0.3}
+          />
+        )}
+        <MeshCircle
+          position={[0, 0, 0]}
+          color={circleColors.center}
+          radius={0.3}
+        />
+        {gameState.rightCircle === "show" && (
+          <MeshCircle
+            position={rightCirclePosition}
+            color={circleColors.sides}
+            radius={0.3}
+          />
+        )}
+      </Canvas>
+      {!isPlaying && (
+        <ConfigCard
+          gameConfig={gameConfig}
+          toggleOrientation={toggleOrientation}
+          startGame={startGame}
+          onChangeRepetitions={onRepetitionChange}
+          onChangeBlinkDuration={onBlinkDurationChange}
+          onChangePauseDuration={onPauseDurationChange}
+        />
+      )}
+    </>
+  );
 }
 
 export default SaccadesModule;
