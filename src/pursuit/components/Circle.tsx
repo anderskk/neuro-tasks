d030265af2de1ade968d8a83058b196d5ace217d
Anders Kristiansen <andersk.kristiansen@gmail.com>
2024-03-20 16:14:27 +0100

    rotate focus cross in circle 45 deg

--- src/pursuit/components/Circle.tsx
+++ src/pursuit/components/Circle.tsx
@@ -6,12 +6,14 @@ interface Props {
 }
 
 export const Circle = ({ radius, className }: Props) => {
-  const classList = cn('rounded-full relative', className);
+  const classList = cn('rounded-full', className);
 
   return (
     <div style={{ width: `${radius}px`, height: `${radius}px`}} className={classList}>
-      <div className="absolute w-[6%] h-full bg-background left-[50%] translate-x-[-50%]" />
-      <div className="absolute h-[6%] w-full bg-background top-[50%] translate-y-[-50%]" />
+      <div className="bg-transparent rotate-45 relative h-full w-full">
+        <div className="absolute w-[6%] h-full bg-background left-[50%] translate-x-[-50%]" />
+        <div className="absolute h-[6%] w-full bg-background top-[50%] translate-y-[-50%]" />
+      </div>
     </div>
   );
 };
